491b9112c7140a1703aa119707b01ad6ecc02a26
rishabhdutta01 <rishabh.dutta01@gmail.com>
2021-04-20 01:56:47 -0400

    changing url again to deploy on heroku

--- src/services/questions-service.js
+++ src/services/questions-service.js
@@ -1,5 +1,5 @@
 //const QUIZZES_URL = 'http://localhost:3001/api/quizzes'
-const QUIZZES_URL = 'http://wbdv-assignment-node-rishabh.herokuapp.com/api/quizzes'
+const QUIZZES_URL = 'https://wbdv-assignment-node-rishabh.herokuapp.com/api/quizzes'
 
 const findQuestionsForQuiz = (quizId) => {
     return fetch(`${QUIZZES_URL}/${quizId}/questions`)
--- src/services/quizzes-service.js
+++ src/services/quizzes-service.js
@@ -1,5 +1,5 @@
 //const QUIZZES_URL = 'http://localhost:3001/api/quizzes'
-const QUIZZES_URL = 'http://wbdv-assignment-node-rishabh.herokuapp.com/api/quizzes'
+const QUIZZES_URL = 'https://wbdv-assignment-node-rishabh.herokuapp.com/api/quizzes'
 
 const findAllQuizzes = () => {
     return fetch(QUIZZES_URL)
